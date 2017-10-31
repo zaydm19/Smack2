ba18309a66c612938f218cda79a21c23570e1059
Zayd Midani <zaydmidani@zayds-macbook-pro.local>
2017-10-31 11:31:47 -0400

    Initial Commit

---
+++ Smack2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack2
+//
+//  Created by Zayd Midani on 2017-10-31.
+//  Copyright © 2017 Zayd Midani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
