96af20bd4c9d8b8bbb0c79e8bab06f932f960ecd
Zayd Midani <zaydmidani@Zayds-MacBook-Pro.local>
2017-11-02 16:52:39 -0400

    completed lesson04

---
+++ Smack2/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack2
+//
+//  Created by Zayd Midani on 2017-11-02.
+//  Copyright © 2017 Zayd Midani. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack2/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack2
+//
+//  Created by Zayd Midani on 2017-11-02.
+//  Copyright © 2017 Zayd Midani. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
