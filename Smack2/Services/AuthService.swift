57511024ec0b2b8cd3e8800ac93413cfa7dde48f
Zayd Midani <zaydmidani@Zayds-MacBook-Pro.local>
2017-11-07 14:02:21 -0500

    lesson06

---
+++ Smack2/Services/AuthService.swift
@@ -0,0 +1,75 @@
+//
+//  AuthService.swift
+//  Smack2
+//
+//  Created by Zayd Midani on 2017-11-07.
+//  Copyright © 2017 Zayd Midani. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken : String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+        
+    }
+    
+    var userEmail : String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "Application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+            completion(true)
+        } else {
+            completion(false)
+            debugPrint(response.result.error as Any)
+        }
+        
+    }
+   
+}
+    
+    
+    
+    
+    
+}
--- Smack2/Utilities/Constants.swift
+++ Smack2/Utilities/Constants.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL constants
+let BASE_URL = "https://chattyz.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
 
