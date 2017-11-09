469d36d8f04af3a34cb0854b93bc4eba3002365f
Zayd Midani <zaydmidani@Zayds-MacBook-Pro.local>
2017-11-09 12:46:35 -0500

    lesson08

--- Smack2/Controller/CreateAccountVC.swift
+++ Smack2/Controller/CreateAccountVC.swift
@@ -30,7 +30,12 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("register user")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("Logged in user!",
+                              AuthService.instance.authToken)
+                    }
+                })
             }
         }
     }
--- Smack2/Utilities/Constants.swift
+++ Smack2/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 // URL constants
 let BASE_URL = "https://chattyz.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //Segues
 let TO_LOGIN = "toLogin"
@@ -25,4 +26,10 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// headers
+
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
 
