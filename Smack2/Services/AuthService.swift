0079dc4bc7f9d6120b3c765cf11ea09943aacf89
Zayd Midani <zaydmidani@Zayds-MacBook-Pro.local>
2017-11-13 10:08:33 -0500

    lesson09

--- Smack2/Services/AuthService.swift
+++ Smack2/Services/AuthService.swift
@@ -85,7 +85,7 @@ class AuthService {
 //                        self.authToken = token
 //                    }
 //                }
-                //versus 
+                //versus
                 
                 // USING SWIFTY JSON
                 guard let data = response.data else {return}
@@ -102,6 +102,74 @@ class AuthService {
             }
         }
     }
+    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler) {
+        
+        let lowercaseEmail = email.lowercased()
+        
+        let header = [
+            "Authorization": "Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+            
+        ]
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowercaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else {return}
+                let json = JSON(data: data)
+                let id = json["_id"].stringValue
+                let color = json["avatarColor"].stringValue
+                let avatarName = json["avatarName"].stringValue
+                let email = json["email"].stringValue
+                let name = json["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                
+                completion(true)
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
    
 }
     
